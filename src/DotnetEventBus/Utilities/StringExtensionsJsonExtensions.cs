fddf2daccd28145995451840c0c786a35f9c12ab
Vladyslav Zaiets <rutova2@gmail.com>
2023-05-30 20:52:11 +0300

    serialize StringExtensionsJson properly

---
+++ src/DotnetEventBus/Utilities/StringExtensionsJsonExtensions.cs
@@ -0,0 +1,80 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =====================================================================
+
+using System;
+using System.Text.Json;
+
+namespace DotnetEventBus.Utilities;
+
+/// <summary>
+/// Provides JSON serialization and deserialization extension methods for strings.
+/// Enables conversion between string values and their JSON representation.
+/// </summary>
+public static class StringExtensionsJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.General)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false
+    };
+
+    /// <summary>
+    /// Serializes a string to a JSON string.
+    /// </summary>
+    /// <param name="value">The string value to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the string value.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this string value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonSerializerOptions) { WriteIndented = true }
+            : _jsonSerializerOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a string.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized string, or null if the JSON is null or empty.</returns>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static string? FromJson(string json)
+    {
+        if (string.IsNullOrEmpty(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<string>(json, _jsonSerializerOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a string.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized string if successful.</param>
+    /// <returns>True if deserialization succeeds; otherwise, false.</returns>
+    public static bool TryFromJson(string json, out string? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<string>(json, _jsonSerializerOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
