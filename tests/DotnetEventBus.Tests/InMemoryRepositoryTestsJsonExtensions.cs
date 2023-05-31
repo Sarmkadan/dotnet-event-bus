fd4bad7bab40e6410a09b662ef8a70e05e87cb86
Vladyslav Zaiets <rutova2@gmail.com>
2023-05-31 14:40:20 +0300

    InMemoryRepositoryTestsJson (de)serialization helpers

---
+++ tests/DotnetEventBus.Tests/InMemoryRepositoryTestsJsonExtensions.cs
@@ -0,0 +1,89 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Text.Json;
+
+namespace DotnetEventBus.Tests;
+
+/// <summary>
+/// Provides JSON serialization and deserialization extensions for <see cref="InMemoryRepositoryTests"/>.
+/// </summary>
+public static class InMemoryRepositoryTestsJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        PropertyNameCaseInsensitive = true
+    };
+
+    /// <summary>
+    /// Serializes the <see cref="InMemoryRepositoryTests"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The repository tests instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation.</param>
+    /// <returns>A JSON string representation of the repository tests.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this InMemoryRepositoryTests value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+            : _jsonOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to an <see cref="InMemoryRepositoryTests"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>An <see cref="InMemoryRepositoryTests"/> instance, or null if the JSON is empty.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static InMemoryRepositoryTests? FromJson(string json)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<InMemoryRepositoryTests>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to an <see cref="InMemoryRepositoryTests"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized instance, or null on failure.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+    public static bool TryFromJson(string json, out InMemoryRepositoryTests? value)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        value = null;
+
+        try
+        {
+            if (!string.IsNullOrWhiteSpace(json))
+            {
+                value = JsonSerializer.Deserialize<InMemoryRepositoryTests>(json, _jsonOptions);
+                return true;
+            }
+
+            return true; // Empty JSON is valid and returns null
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
