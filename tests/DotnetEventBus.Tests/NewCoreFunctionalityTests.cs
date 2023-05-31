84e3c9851aa66eca02912856c9c3e99261272701
Vladyslav Zaiets <rutova2@gmail.com>
2023-05-31 19:18:45 +0300

    small refactor - NewCoreFunctionalityTests
    should be backwards compatible.

--- tests/DotnetEventBus.Tests/NewCoreFunctionalityTests.cs
+++ tests/DotnetEventBus.Tests/NewCoreFunctionalityTests.cs
@@ -11,10 +11,19 @@ using FluentAssertions;
 
 namespace DotnetEventBus.Tests;
 
+/// <summary>
+/// Contains integration tests for new core functionality of the DotnetEventBus library.
+/// Tests priority-based handler invocation, middleware pipeline execution, retry mechanisms,
+/// dead letter queue functionality, and subscription management.
+/// </summary>
 public class NewCoreFunctionalityTests
 {
     private readonly IServiceProvider _serviceProvider;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NewCoreFunctionalityTests"/> class.
+    /// Sets up the test environment with a service provider for dependency injection.
+    /// </summary>
     public NewCoreFunctionalityTests()
     {
         var services = new ServiceCollection();
@@ -22,6 +31,10 @@ public class NewCoreFunctionalityTests
         _serviceProvider = services.BuildServiceProvider();
     }
 
+    /// <summary>
+    /// Tests that event handlers are invoked in priority order when multiple handlers
+    /// are subscribed to the same event type.
+    /// </summary>
     [Fact]
     public async Task PublishAsync_ShouldInvokeHandlersInPriorityOrder()
     {
@@ -40,6 +53,10 @@ public class NewCoreFunctionalityTests
         invocationOrder.Should().ContainInOrder("High", "Low");
     }
 
+    /// <summary>
+    /// Tests that middleware pipeline is executed when publishing events.
+    /// Verifies that middleware can modify the context and that the pipeline completes successfully.
+    /// </summary>
     [Fact]
     public async Task PublishAsync_ShouldExecuteMiddlewarePipeline()
     {
@@ -54,9 +71,9 @@ public class NewCoreFunctionalityTests
         var services = new ServiceCollection();
         services.AddSingleton(middlewareMock.Object);
         var provider = services.BuildServiceProvider();
-        
+
         options.MiddlewareTypes.Add(typeof(IEventBusMiddleware));
-        
+
         var bus = new EventBus(options: options, serviceProvider: provider);
 
         // Act
@@ -66,6 +83,10 @@ public class NewCoreFunctionalityTests
         middlewareMock.Verify(m => m.InvokeAsync(It.IsAny<EventMiddlewareContext>(), It.IsAny<EventMiddlewareDelegate>()), Times.Once);
     }
 
+    /// <summary>
+    /// Tests that failed event handlers are automatically retried according to configured retry policy.
+    /// Verifies that the retry mechanism executes the handler multiple times before giving up.
+    /// </summary>
     [Fact]
     public async Task PublishAsync_ShouldRetryFailedHandlers()
     {
@@ -87,23 +108,27 @@ public class NewCoreFunctionalityTests
         result.Success.Should().BeFalse();
     }
 
+    /// <summary>
+    /// Tests that failed event handlers are moved to the dead letter queue when configured.
+    /// Verifies that handlers that consistently fail are properly tracked and stored for later inspection.
+    /// </summary>
     [Fact]
     public async Task PublishAsync_ShouldMoveFailedHandlerToDeadLetterQueue()
     {
         // Arrange
         var dlqRepository = new InMemoryDeadLetterRepository();
-        var options = new EventBusOptions 
-        { 
-            MaxRetryAttempts = 0, 
-            EnableDeadLetterQueue = true 
+        var options = new EventBusOptions
+        {
+            MaxRetryAttempts = 0,
+            EnableDeadLetterQueue = true
         };
-        
+
         var services = new ServiceCollection();
         services.AddLogging();
         services.AddSingleton<IDeadLetterRepository>(dlqRepository);
         // We need a proper way to inject the repository into the constructor.
         // Looking at the constructor, it seems I need to pass it explicitly in the other overload.
-        
+
         var bus = new EventBus(
             new InMemoryEventMessageRepository(),
             new InMemorySubscriptionRepository(),
@@ -123,13 +148,17 @@ public class NewCoreFunctionalityTests
         deadLetters.Should().ContainSingle(e => e.FailedHandlerName == "BadHandler");
     }
 
+    /// <summary>
+    /// Tests that unsubscribing prevents future invocations of the handler.
+    /// Verifies that subscription cleanup works correctly and handlers are not called after disposal.
+    /// </summary>
     [Fact]
     public async Task Unsubscribe_ShouldPreventFutureInvocations()
     {
         // Arrange
         var bus = new EventBus(options: new EventBusOptions(), serviceProvider: _serviceProvider);
         int callCount = 0;
-        
+
         var subscription = bus.SubscribeSync<string>(e => callCount++);
 
         // Act
