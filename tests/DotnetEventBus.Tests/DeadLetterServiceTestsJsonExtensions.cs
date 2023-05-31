e6cd0e18355dedc756960cbf4185e650647cfaa5
Vladyslav Zaiets <rutova2@gmail.com>
2023-05-31 10:38:15 +0300

    Serialize DeadLetterServiceTestsJson properly

---
+++ tests/DotnetEventBus.Tests/DeadLetterServiceTestsJsonExtensions.cs
@@ -0,0 +1,84 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =====================================================================
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
+
+namespace DotnetEventBus.Tests;
+
+/// <summary>
+/// Provides System.Text.Json serialization and deserialization extensions for DeadLetterServiceTests.
+/// </summary>
+public static class DeadLetterServiceTestsJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        TypeInfoResolver = new DefaultJsonTypeInfoResolver()
+    };
+
+    /// <summary>
+    /// Serializes a DeadLetterServiceTests instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this DeadLetterServiceTests value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+            : _jsonOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a DeadLetterServiceTests instance from a JSON string.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized instance, or null if the JSON is empty or whitespace.</returns>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static DeadLetterServiceTests? FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<DeadLetterServiceTests>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a DeadLetterServiceTests instance from a JSON string.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">The deserialized instance, or null if deserialization fails.</param>
+    /// <returns>True if deserialization succeeds; otherwise, false.</returns>
+    public static bool TryFromJson(string json, out DeadLetterServiceTests? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<DeadLetterServiceTests>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
