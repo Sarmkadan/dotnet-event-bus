e7e194d986cb572b7139bbdcd78fb104ba2d9ac3
Vladyslav Zaiets <rutova2@gmail.com>
2023-05-31 09:21:43 +0300

    CsvEventFormatterJson (de)serialization helpers.

---
+++ src/DotnetEventBus/Formatters/CsvEventFormatterJsonExtensions.cs
@@ -0,0 +1,88 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =====================================================================
+
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DotnetEventBus.Formatters;
+
+/// <summary>
+/// Provides System.Text.Json serialization and deserialization extensions for <see cref="CsvEventFormatter"/>.
+/// Enables JSON representation of CSV formatter configuration and serialized event data.
+/// </summary>
+public static class CsvEventFormatterJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+    };
+
+    /// <summary>
+    /// Serializes the <see cref="CsvEventFormatter"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The formatter instance to serialize. Cannot be null.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the formatter.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this CsvEventFormatter value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+            : _jsonOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="CsvEventFormatter"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize. Cannot be null or empty.</param>
+    /// <returns>A deserialized <see cref="CsvEventFormatter"/> instance, or null if deserialization fails.</returns>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    public static CsvEventFormatter? FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            return JsonSerializer.Deserialize<CsvEventFormatter>(json, _jsonOptions);
+        }
+        catch (JsonException)
+        {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="CsvEventFormatter"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize. Cannot be null or empty.</param>
+    /// <param name="value">Receives the deserialized formatter if successful; otherwise, null.</param>
+    /// <returns>True if deserialization succeeds; otherwise, false.</returns>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    public static bool TryFromJson(string json, out CsvEventFormatter? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<CsvEventFormatter>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
