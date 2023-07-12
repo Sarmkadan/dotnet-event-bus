05769edde3c0c4d043e1a90fa152f2329ed0434b
Vladyslav Zaiets <rutova2@gmail.com>
2023-07-12 12:29:32 +0300

    feat: handy extensions for EventBusBenchmarks

--- benchmarks/DotnetEventBus.Benchmarks/Benchmarks/EventBusBenchmarksExtensions.cs
+++ benchmarks/DotnetEventBus.Benchmarks/Benchmarks/EventBusBenchmarksExtensions.cs
@@ -3,6 +3,10 @@ using System.Threading.Tasks;
 
 namespace DotnetEventBus.Benchmarks
 {
+    /// <summary>
+    /// Provides extension methods for fluent configuration of <see cref="EventBusBenchmarks"/> instances.
+    /// Enables chaining of setup, configuration, and cleanup operations in a fluent API style.
+    /// </summary>
     public static class EventBusBenchmarksExtensions
     {
         /// <summary>
@@ -11,6 +15,7 @@ namespace DotnetEventBus.Benchmarks
         /// <param name="id">The unique identifier for this benchmark instance.</param>
         /// <param name="name">The descriptive name for this benchmark.</param>
         /// <returns>A new EventBusBenchmarks instance configured with the provided values.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="benchmarks"/> is <see langword="null"/></exception>
         public static EventBusBenchmarks WithIdentity(this EventBusBenchmarks benchmarks, string id, string name)
         {
             if (benchmarks == null)
@@ -32,6 +37,7 @@ namespace DotnetEventBus.Benchmarks
         /// </summary>
         /// <param name="value">The new value to set.</param>
         /// <returns>A new EventBusBenchmarks instance with the updated value.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="benchmarks"/> is <see langword="null"/></exception>
         public static EventBusBenchmarks WithValue(this EventBusBenchmarks benchmarks, int value)
         {
             if (benchmarks == null)
@@ -52,7 +58,9 @@ namespace DotnetEventBus.Benchmarks
         /// Executes the GlobalSetup method and returns the configured benchmark instance.
         /// This is useful for chaining setup operations in fluent scenarios.
         /// </summary>
+        /// <param name="benchmarks">The benchmark instance to configure.</param>
         /// <returns>The benchmark instance after setup.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="benchmarks"/> is <see langword="null"/></exception>
         public static EventBusBenchmarks WithSetup(this EventBusBenchmarks benchmarks)
         {
             if (benchmarks == null)
@@ -66,7 +74,9 @@ namespace DotnetEventBus.Benchmarks
         /// Executes the GlobalCleanup method and returns the configured benchmark instance.
         /// This is useful for chaining cleanup operations in fluent scenarios.
         /// </summary>
+        /// <param name="benchmarks">The benchmark instance to configure.</param>
         /// <returns>The benchmark instance after cleanup.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="benchmarks"/> is <see langword="null"/></exception>
         public static EventBusBenchmarks WithCleanup(this EventBusBenchmarks benchmarks)
         {
             if (benchmarks == null)
