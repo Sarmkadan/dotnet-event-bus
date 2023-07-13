66bfdde47ff800e0e75506a33d5ea1fa43f4ead7
Vladyslav Zaiets <rutova2@gmail.com>
2023-07-13 06:10:57 +0300

    feat: more PredicateSubscriptionBuilder extension methods

--- src/DotnetEventBus/Handlers/PredicateSubscriptionBuilderExtensions.cs
+++ src/DotnetEventBus/Handlers/PredicateSubscriptionBuilderExtensions.cs
@@ -24,15 +24,14 @@ public static class PredicateSubscriptionBuilderExtensions
     /// <param name="builder">The builder instance.</param>
     /// <param name="predicate">The condition to evaluate against each published event.</param>
     /// <returns>The builder instance for fluent chaining.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="builder"/> or <paramref name="predicate"/> is <see langword="null"/>.</exception>
     public static PredicateSubscriptionBuilder<TEvent> Where<TEvent>(
         this PredicateSubscriptionBuilder<TEvent> builder,
         Expression<Func<TEvent, bool>> predicate)
         where TEvent : class
     {
-        if (builder is null)
-            throw new ArgumentNullException(nameof(builder));
-        if (predicate is null)
-            throw new ArgumentNullException(nameof(predicate));
+        ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(predicate);
 
         return builder.Where(predicate.Compile());
     }
@@ -46,15 +45,14 @@ public static class PredicateSubscriptionBuilderExtensions
     /// <param name="builder">The builder instance.</param>
     /// <param name="predicate">The condition to negate.</param>
     /// <returns>The builder instance for fluent chaining.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="builder"/> or <paramref name="predicate"/> is <see langword="null"/>.</exception>
     public static PredicateSubscriptionBuilder<TEvent> WhereNot<TEvent>(
         this PredicateSubscriptionBuilder<TEvent> builder,
         Expression<Func<TEvent, bool>> predicate)
         where TEvent : class
     {
-        if (builder is null)
-            throw new ArgumentNullException(nameof(builder));
-        if (predicate is null)
-            throw new ArgumentNullException(nameof(predicate));
+        ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(predicate);
 
         return builder.WhereNot(predicate.Compile());
     }
@@ -67,13 +65,12 @@ public static class PredicateSubscriptionBuilderExtensions
     /// <typeparam name="TDerived">The derived event type to match.</typeparam>
     /// <param name="builder">The builder instance.</param>
     /// <returns>The builder instance for fluent chaining.</returns>
-    public static PredicateSubscriptionBuilder<TEvent> WhereTypeIs<TEvent, TDerived>(
-        this PredicateSubscriptionBuilder<TEvent> builder)
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="builder"/> is <see langword="null"/>.</exception>
+    public static PredicateSubscriptionBuilder<TEvent> WhereTypeIs<TEvent, TDerived>(this PredicateSubscriptionBuilder<TEvent> builder)
         where TEvent : class
         where TDerived : class, TEvent
     {
-        if (builder is null)
-            throw new ArgumentNullException(nameof(builder));
+        ArgumentNullException.ThrowIfNull(builder);
 
         return builder.Where(e => e is TDerived);
     }
@@ -86,13 +83,12 @@ public static class PredicateSubscriptionBuilderExtensions
     /// <typeparam name="TExcluded">The derived event type to exclude.</typeparam>
     /// <param name="builder">The builder instance.</param>
     /// <returns>The builder instance for fluent chaining.</returns>
-    public static PredicateSubscriptionBuilder<TEvent> WhereTypeIsNot<TEvent, TExcluded>(
-        this PredicateSubscriptionBuilder<TEvent> builder)
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="builder"/> is <see langword="null"/>.</exception>
+    public static PredicateSubscriptionBuilder<TEvent> WhereTypeIsNot<TEvent, TExcluded>(this PredicateSubscriptionBuilder<TEvent> builder)
         where TEvent : class
         where TExcluded : class, TEvent
     {
-        if (builder is null)
-            throw new ArgumentNullException(nameof(builder));
+        ArgumentNullException.ThrowIfNull(builder);
 
         return builder.WhereNot(e => e is TExcluded);
     }
@@ -107,17 +103,18 @@ public static class PredicateSubscriptionBuilderExtensions
     /// <param name="propertySelector">Selector that extracts the property from the event.</param>
     /// <param name="allowedValues">The set of allowed values for the property.</param>
     /// <returns>The builder instance for fluent chaining.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="builder"/>, <paramref name="propertySelector"/>, or <paramref name="allowedValues"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="allowedValues"/> is empty.</exception>
     public static PredicateSubscriptionBuilder<TEvent> WherePropertyIn<TEvent, TProperty>(
         this PredicateSubscriptionBuilder<TEvent> builder,
         Func<TEvent, TProperty> propertySelector,
         params TProperty[] allowedValues)
         where TEvent : class
     {
-        if (builder is null)
-            throw new ArgumentNullException(nameof(builder));
-        if (propertySelector is null)
-            throw new ArgumentNullException(nameof(propertySelector));
-        if (allowedValues is null || allowedValues.Length == 0)
+        ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(propertySelector);
+        ArgumentNullException.ThrowIfNull(allowedValues);
+        if (allowedValues.Length == 0)
             throw new ArgumentException("At least one allowed value must be provided.", nameof(allowedValues));
 
         return builder.Where(e => allowedValues.Contains(propertySelector(e)));
@@ -131,15 +128,14 @@ public static class PredicateSubscriptionBuilderExtensions
     /// <param name="builder">The builder instance.</param>
     /// <param name="propertySelector">Selector that extracts the property from the event.</param>
     /// <returns>The builder instance for fluent chaining.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="builder"/> or <paramref name="propertySelector"/> is <see langword="null"/>.</exception>
     public static PredicateSubscriptionBuilder<TEvent> WherePropertyIsNull<TEvent, TProperty>(
         this PredicateSubscriptionBuilder<TEvent> builder,
         Func<TEvent, TProperty> propertySelector)
         where TEvent : class
     {
-        if (builder is null)
-            throw new ArgumentNullException(nameof(builder));
-        if (propertySelector is null)
-            throw new ArgumentNullException(nameof(propertySelector));
+        ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(propertySelector);
 
         return builder.Where(e => propertySelector(e) is null);
     }
@@ -152,15 +148,14 @@ public static class PredicateSubscriptionBuilderExtensions
     /// <param name="builder">The builder instance.</param>
     /// <param name="propertySelector">Selector that extracts the property from the event.</param>
     /// <returns>The builder instance for fluent chaining.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="builder"/> or <paramref name="propertySelector"/> is <see langword="null"/>.</exception>
     public static PredicateSubscriptionBuilder<TEvent> WherePropertyIsNotNull<TEvent, TProperty>(
         this PredicateSubscriptionBuilder<TEvent> builder,
         Func<TEvent, TProperty> propertySelector)
         where TEvent : class
     {
-        if (builder is null)
-            throw new ArgumentNullException(nameof(builder));
-        if (propertySelector is null)
-            throw new ArgumentNullException(nameof(propertySelector));
+        ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(propertySelector);
 
         return builder.Where(e => propertySelector(e) is not null);
     }
@@ -173,18 +168,16 @@ public static class PredicateSubscriptionBuilderExtensions
     /// <param name="propertySelector">Selector that extracts the string property from the event.</param>
     /// <param name="pattern">The regular expression pattern to match.</param>
     /// <returns>The builder instance for fluent chaining.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="builder"/>, <paramref name="propertySelector"/>, or <paramref name="pattern"/> is <see langword="null"/>.</exception>
     public static PredicateSubscriptionBuilder<TEvent> WherePropertyMatches<TEvent>(
         this PredicateSubscriptionBuilder<TEvent> builder,
         Func<TEvent, string?> propertySelector,
         string pattern)
         where TEvent : class
     {
-        if (builder is null)
-            throw new ArgumentNullException(nameof(builder));
-        if (propertySelector is null)
-            throw new ArgumentNullException(nameof(propertySelector));
-        if (pattern is null)
-            throw new ArgumentNullException(nameof(pattern));
+        ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(propertySelector);
+        ArgumentNullException.ThrowIfNull(pattern);
 
         var regex = new System.Text.RegularExpressions.Regex(pattern, System.Text.RegularExpressions.RegexOptions.IgnoreCase);
         return builder.Where(e => propertySelector(e) is not null && regex.IsMatch(propertySelector(e)!));
@@ -200,6 +193,7 @@ public static class PredicateSubscriptionBuilderExtensions
     /// <param name="minValue">The minimum allowed value (inclusive).</param>
     /// <param name="maxValue">The maximum allowed value (inclusive).</param>
     /// <returns>The builder instance for fluent chaining.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="builder"/> or <paramref name="propertySelector"/> is <see langword="null"/>.</exception>
     public static PredicateSubscriptionBuilder<TEvent> WherePropertyInRange<TEvent, TProperty>(
         this PredicateSubscriptionBuilder<TEvent> builder,
         Func<TEvent, TProperty> propertySelector,
@@ -208,10 +202,8 @@ public static class PredicateSubscriptionBuilderExtensions
         where TEvent : class
         where TProperty : IComparable<TProperty>
     {
-        if (builder is null)
-            throw new ArgumentNullException(nameof(builder));
-        if (propertySelector is null)
-            throw new ArgumentNullException(nameof(propertySelector));
+        ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(propertySelector);
 
         return builder.Where(e =>
         {
@@ -228,20 +220,19 @@ public static class PredicateSubscriptionBuilderExtensions
     /// <param name="handlerName">A descriptive name that identifies this subscription.</param>
     /// <param name="handler">The async delegate that processes the event.</param>
     /// <returns>The builder instance for fluent chaining.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="builder"/>, <paramref name="handlerName"/>, or <paramref name="handler"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="handlerName"/> is empty or whitespace.</exception>
     public static PredicateSubscriptionBuilder<TEvent> WithHandler<TEvent>(
         this PredicateSubscriptionBuilder<TEvent> builder,
         string handlerName,
         Func<TEvent, CancellationToken, Task> handler)
         where TEvent : class
     {
-        if (builder is null)
-            throw new ArgumentNullException(nameof(builder));
-        if (handlerName is null)
-            throw new ArgumentNullException(nameof(handlerName));
+        ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(handlerName);
         if (string.IsNullOrWhiteSpace(handlerName))
-            throw new ArgumentException("Handler name cannot be empty.", nameof(handlerName));
-        if (handler is null)
-            throw new ArgumentNullException(nameof(handler));
+            throw new ArgumentException("Handler name cannot be empty or whitespace.", nameof(handlerName));
+        ArgumentNullException.ThrowIfNull(handler);
 
         return builder.WithHandlerName(handlerName).WithHandler(handler);
     }
@@ -254,18 +245,16 @@ public static class PredicateSubscriptionBuilderExtensions
     /// <param name="loggerFactory">The logger factory to create the logger.</param>
     /// <param name="categoryName">The category name for the logger.</param>
     /// <returns>The builder instance for fluent chaining.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="builder"/>, <paramref name="loggerFactory"/>, or <paramref name="categoryName"/> is <see langword="null"/>.</exception>
     public static PredicateSubscriptionBuilder<TEvent> WithLogger<TEvent>(
         this PredicateSubscriptionBuilder<TEvent> builder,
         ILoggerFactory loggerFactory,
         string categoryName)
         where TEvent : class
     {
-        if (builder is null)
-            throw new ArgumentNullException(nameof(builder));
-        if (loggerFactory is null)
-            throw new ArgumentNullException(nameof(loggerFactory));
-        if (categoryName is null)
-            throw new ArgumentNullException(nameof(categoryName));
+        ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(loggerFactory);
+        ArgumentNullException.ThrowIfNull(categoryName);
 
         return builder.WithLogger(loggerFactory.CreateLogger(categoryName));
     }
