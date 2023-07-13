0cbedbe6b368b20820c4376627a1d9421beafb1b
Vladyslav Zaiets <rutova2@gmail.com>
2023-07-13 16:09:38 +0300

    feat(advanced): more EventTransformer extension methods

--- src/DotnetEventBus/Advanced/EventTransformerExtensions.cs
+++ src/DotnetEventBus/Advanced/EventTransformerExtensions.cs
@@ -14,15 +14,22 @@ namespace DotnetEventBus.Advanced;
 /// <summary>
 /// Extension methods for <see cref="EventTransformer{TSource, TTarget}"/> providing common transformation patterns.
 /// </summary>
+/// <remarks>
+/// All methods validate arguments and throw <see cref="ArgumentNullException"/> for null inputs.
+/// Methods use expression-bodied syntax where appropriate for idiomatic C#.
+/// </remarks>
 public static class EventTransformerExtensions
 {
     /// <summary>
     /// Transforms the event and applies a conditional post-transformation if the condition is met.
     /// </summary>
+    /// <typeparam name="TSource">Source event type</typeparam>
+    /// <typeparam name="TTarget">Target event type</typeparam>
     /// <param name="transformer">The transformer instance</param>
     /// <param name="condition">Function that determines if the post-transformation should be applied</param>
-    /// <param name="postTransform">Transformation to apply when condition is true</param>
+    /// <param name="postTransform">Transformation to apply when condition is <see langword="true"/></param>
     /// <returns>The transformer for fluent chaining</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="transformer"/>, <paramref name="condition"/>, or <paramref name="postTransform"/> is <see langword="null"/></exception>
     public static EventTransformer<TSource, TTarget> When<TSource, TTarget>(
         this EventTransformer<TSource, TTarget> transformer,
         Func<TTarget, bool> condition,
@@ -40,9 +47,12 @@ public static class EventTransformerExtensions
     /// <summary>
     /// Transforms the event and applies a transformation only if the target is not null.
     /// </summary>
+    /// <typeparam name="TSource">Source event type</typeparam>
+    /// <typeparam name="TTarget">Target event type</typeparam>
     /// <param name="transformer">The transformer instance</param>
     /// <param name="postTransform">Transformation to apply when target is not null</param>
     /// <returns>The transformer for fluent chaining</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="transformer"/> or <paramref name="postTransform"/> is <see langword="null"/></exception>
     public static EventTransformer<TSource, TTarget> IfNotNull<TSource, TTarget>(
         this EventTransformer<TSource, TTarget> transformer,
         Func<TTarget, TTarget> postTransform)
@@ -58,9 +68,12 @@ public static class EventTransformerExtensions
     /// <summary>
     /// Transforms the event and applies multiple post-transformations in sequence.
     /// </summary>
+    /// <typeparam name="TSource">Source event type</typeparam>
+    /// <typeparam name="TTarget">Target event type</typeparam>
     /// <param name="transformer">The transformer instance</param>
     /// <param name="postTransforms">Sequence of transformations to apply</param>
     /// <returns>The transformer for fluent chaining</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="transformer"/> or <paramref name="postTransforms"/> is <see langword="null"/></exception>
     public static EventTransformer<TSource, TTarget> ThenAll<TSource, TTarget>(
         this EventTransformer<TSource, TTarget> transformer,
         params Func<TTarget, TTarget>[] postTransforms)
@@ -90,10 +103,14 @@ public static class EventTransformerExtensions
     /// Transforms the event and applies a transformation that maps to a different type,
     /// then converts back to the original target type.
     /// </summary>
+    /// <typeparam name="TSource">Source event type</typeparam>
+    /// <typeparam name="TTarget">Target event type</typeparam>
+    /// <typeparam name="TIntermediate">Intermediate transformation type</typeparam>
     /// <param name="transformer">The transformer instance</param>
     /// <param name="intermediateTransform">Transformation to apply to create intermediate type</param>
     /// <param name="finalTransform">Transformation to convert back to target type</param>
     /// <returns>The transformer for fluent chaining</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="transformer"/>, <paramref name="intermediateTransform"/>, or <paramref name="finalTransform"/> is <see langword="null"/></exception>
     public static EventTransformer<TSource, TTarget> MapIntermediate<TSource, TTarget, TIntermediate>(
         this EventTransformer<TSource, TTarget> transformer,
         Func<TTarget, TIntermediate> intermediateTransform,
