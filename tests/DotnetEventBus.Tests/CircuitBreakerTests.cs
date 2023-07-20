14c4c52ad11593d11b73235f247fac6ef32bf7b5
Vladyslav Zaiets <rutova2@gmail.com>
2023-07-20 08:03:17 +0300

    polish CircuitBreakeTrests
    should be backwards compatible.

--- tests/DotnetEventBus.Tests/CircuitBreakerTests.cs
+++ tests/DotnetEventBus.Tests/CircuitBreakerTests.cs
@@ -6,8 +6,17 @@ using DotnetEventBus.Integration;
 
 namespace DotnetEventBus.Tests;
 
+/// <summary>
+/// Contains unit tests for the <see cref="CircuitBreaker"/> class.
+/// Tests circuit breaker behavior including state transitions, failure handling,
+/// and recovery mechanisms.
+/// </summary>
 public sealed class CircuitBreakerTests
 {
+    /// <summary>
+    /// Tests that a successful operation completes normally and circuit remains closed.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
     [Fact]
     public async Task ExecuteAsync_WithSuccessfulOperation_ShouldReturnResult()
     {
@@ -25,6 +34,11 @@ public sealed class CircuitBreakerTests
         breaker.State.Should().Be(CircuitBreakerState.Closed);
     }
 
+    /// <summary>
+    /// Tests that exceptions below the failure threshold don't open the circuit.
+    /// The breaker should continue processing operations normally.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
     [Fact]
     public async Task ExecuteAsync_WithExceptionsBelowThreshold_ShouldContinueProcessing()
     {
@@ -48,6 +62,11 @@ public sealed class CircuitBreakerTests
         executionCount.Should().Be(3);
     }
 
+    /// <summary>
+    /// Tests that exceeding the failure threshold transitions the circuit to the Open state.
+    /// Once the threshold is exceeded, the breaker should stop processing operations.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
     [Fact]
     public async Task ExecuteAsync_WhenExceptionsExceedThreshold_ShouldTransitionToOpen()
     {
@@ -70,6 +89,11 @@ public sealed class CircuitBreakerTests
         breaker.State.Should().Be(CircuitBreakerState.Open);
     }
 
+    /// <summary>
+    /// Tests that attempting to execute when the circuit is Open throws <see cref="CircuitBreakerOpenException"/>.
+    /// This verifies that the breaker prevents operations when in the Open state.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
     [Fact]
     public async Task ExecuteAsync_WhenCircuitIsOpen_ShouldThrowCircuitBreakerOpenException()
     {
@@ -96,6 +120,11 @@ public sealed class CircuitBreakerTests
         await openAct.Should().ThrowAsync<CircuitBreakerOpenException>();
     }
 
+    /// <summary>
+    /// Tests that after the timeout expires, the circuit transitions to HalfOpen state.
+    /// A successful operation in HalfOpen state should close the circuit again.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
     [Fact]
     public async Task ExecuteAsync_AfterTimeoutExpires_ShouldTransitionToHalfOpen()
     {
@@ -126,6 +155,11 @@ public sealed class CircuitBreakerTests
         breaker.State.Should().Be(CircuitBreakerState.Closed);
     }
 
+    /// <summary>
+    /// Tests that a void operation (no return value) completes successfully when the circuit is closed.
+    /// Verifies that both void and non-void operations are handled correctly.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
     [Fact]
     public async Task ExecuteAsync_VoidOperation_WithSuccess_ShouldCompleteSuccessfully()
     {
@@ -145,6 +179,11 @@ public sealed class CircuitBreakerTests
         breaker.State.Should().Be(CircuitBreakerState.Closed);
     }
 
+    /// <summary>
+    /// Tests that a void operation throws <see cref="CircuitBreakerOpenException"/> when the circuit is Open.
+    /// Verifies that both void and non-void operations are protected by the circuit breaker.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
     [Fact]
     public async Task ExecuteAsync_VoidOperation_WhenCircuitIsOpen_ShouldThrow()
     {
@@ -171,6 +210,10 @@ public sealed class CircuitBreakerTests
         await openAct.Should().ThrowAsync<CircuitBreakerOpenException>();
     }
 
+    /// <summary>
+    /// Tests that the constructor throws <see cref="ArgumentException"/> when an invalid failure threshold is provided.
+    /// Validates that the failure threshold must be a positive integer.
+    /// </summary>
     [Fact]
     public void Constructor_WithInvalidFailureThreshold_ShouldThrowArgumentException()
     {
@@ -182,6 +225,11 @@ public sealed class CircuitBreakerTests
         act2.Should().Throw<ArgumentException>();
     }
 
+    /// <summary>
+    /// Tests that passing a null operation to ExecuteAsync throws <see cref="ArgumentNullException"/>.
+    /// Validates proper null checking for the operation parameter.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
     [Fact]
     public async Task ExecuteAsync_WithNullOperation_ShouldThrowArgumentNullException()
     {
@@ -193,6 +241,11 @@ public sealed class CircuitBreakerTests
         await act.Should().ThrowAsync<ArgumentNullException>();
     }
 
+    /// <summary>
+    /// Tests that passing a null void operation to ExecuteAsync throws <see cref="ArgumentNullException"/>.
+    /// Validates proper null checking for the void operation parameter.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
     [Fact]
     public async Task ExecuteAsync_VoidWithNullOperation_ShouldThrowArgumentNullException()
     {
@@ -204,6 +257,11 @@ public sealed class CircuitBreakerTests
         await act.Should().ThrowAsync<ArgumentNullException>();
     }
 
+    /// <summary>
+    /// Tests that a successful operation after some failures resets the circuit to Closed state.
+    /// This verifies the circuit breaker's recovery mechanism works correctly.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
     [Fact]
     public async Task ExecuteAsync_SuccessfulOperationAfterFailures_ShouldResetCircuit()
     {
@@ -235,6 +293,11 @@ public sealed class CircuitBreakerTests
         executionCount.Should().Be(3);
     }
 
+    /// <summary>
+    /// Tests that a failure during HalfOpen state transitions the circuit back to Open state.
+    /// This verifies the circuit breaker properly handles failures during the recovery attempt.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
     [Fact]
     public async Task ExecuteAsync_HalfOpenToOpenTransition_OnFailure()
     {
