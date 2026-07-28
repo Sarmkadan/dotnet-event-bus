c9777cb223a547662ea6f19aceabe3cc0ca92296
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-28 21:30:00 +0300

    EventMessageExtensionsTests changes

---
+++ tests/DotnetEventBus.Tests/EventMessageExtensionsTests.cs
@@ -0,0 +1,62 @@
+using Xunit;
+using DotnetEventBus.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Moq;
+
+namespace DotnetEventBus.Tests
+{
+    public class EventMessageExtensionsTests
+    {
+        [Fact]
+        public void Clone_Happy_PATH_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void HasExceededMaxAttempts_HAPPY_PATH_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void AddHeaders_HAPPY_PATH_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void GetHeaderOrDefault_HAPPY_PATH_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void Clone_NULL_INPUT_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void HasExceededMaxAttempts_NULL_INPUT_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void AddHeaders_NULL_INPUT_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void GetHeaderOrDefault_NULL_INPUT_Test()
+        {
+            // Test implementation here
+        }
+    }
+}
