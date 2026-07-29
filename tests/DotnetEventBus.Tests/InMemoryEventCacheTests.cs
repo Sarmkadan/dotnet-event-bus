a410a0eb14b64590e9685fb8994297827485e877
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-29 19:49:47 +0300

    fix the build

---
+++ tests/DotnetEventBus.Tests/InMemoryEventCacheTests.cs
@@ -0,0 +1,59 @@
+using Xunit;
+using DotnetEventBus;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Moq;
+
+namespace DotnetEventBus.Tests
+{
+    public class InMemoryEventCacheTests
+    {
+        [Fact]
+        public async Task TestInMemoryEventCache_GetAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestInMemoryEventCache_SetAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestInMemoryEventCache_RemoveAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestInMemoryEventCache_ExistsAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestInMemoryEventCache_GetManyAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestInMemoryEventCache_RemoveManyAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestInMemoryEventCache_ClearAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestInMemoryEventCache_GetStatsAsync()
+        {
+            // Test implementation here
+        }
+    }
+}
