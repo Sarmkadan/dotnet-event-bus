e13058bdae81f2bbbd9af357bff59c5748e09016
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 06:21:30 +0300

    fix(dotneteventbus.tests): fix the build

---
+++ tests/DotnetEventBus.Tests/HealthCheckValidationTests.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using DotnetEventBus.Monitoring;
+
+namespace DotnetEventBus.Tests
+{
+    public class HealthCheckValidationTests
+    {
+        [Fact]
+        public void Validate_Happy_Path()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void IsValid_Happy_Path()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void EnsureValid_Happy_Path()
+        {
+            // Test implementation here
+        }
+    }
+}
